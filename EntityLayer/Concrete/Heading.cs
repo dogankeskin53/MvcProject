2cc21bf3307a22176187b080041af49f705e8305
Doğan Keskin <dogankeskin53@gmail.com>
2021-04-12 19:14:43 +0300

    2. Commit

---
+++ EntityLayer/Concrete/Heading.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Heading
+    {
+        //veritabanına yansıyacak olan sutün isimleri
+        public int HeadingId { get; set; }
+        public string HeadingName { get; set; }
+        public DateTime HeadingDate { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/Writer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Writer
+    {
+        public int WriterId { get; set; }
+        public int WriterName { get; set; }
+        public int WriterSurName { get; set; }
+        public int WriterMail { get; set; }
+        public int WriterPassword { get; set; }
+    }
+}
