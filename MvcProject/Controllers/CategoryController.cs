17e489e9da0cf0009f98ec27dedc2418a58bf4d1
Doğan Keskin <dogankeskin53@gmail.com>
2021-04-30 15:46:07 +0300

    Kategori Verilerinin Listelenmesi

--- MvcProject/Controllers/CategoryController.cs
+++ MvcProject/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,28 @@ namespace MvcProject.Controllers
 {
     public class CategoryController : Controller
     {
+
         // GET: Category
+        /// <summary>
+        /// Business sınıfından Category manager çağrılır.
+        /// </summary>
+        /// <returns></returns>
+        CategoryManager cm = new CategoryManager();
+
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        /// <summary>
+        /// View eklemesi yapıyoruz. Göstermek için.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetCategoryList()
+        {
+            var values = cm.GetAllBL();
+            return View(values);
         }
     }
 }
