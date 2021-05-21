3c884505ed22a7b68f398d8059f613c27bfbcc12
Doğan Keskin <dogankeskin53@gmail.com>
2021-05-21 15:20:42 +0300

    Admin Layout Tema eklenmesi
    İlk temamız eklendi ve temaya göre yeni kategori sayfasında düzenleme yapıldı.

---
+++ MvcProject/Controllers/AdminCategoryController.cs
@@ -0,0 +1,57 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.FluentValidation;
+using DataAccesLayer.EntityFrameWork;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProject.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        CategoryManager categoryManager = new CategoryManager(new EFCategoryDal());
+        // GET: AdminCategory
+        public ActionResult Index()
+        {
+            var categoryValues = categoryManager.Getlist();
+            return View(categoryValues);
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+            CategoryValidatior categoryValidator = new CategoryValidatior();
+            ValidationResult validationResult = categoryValidator.Validate(p);
+
+            /// eğer kural geçerli ise
+            if (validationResult.IsValid)
+            {
+                categoryManager.CategortAdd(p);
+                //action adına indexe yönlendir
+                return RedirectToAction("Index"); 
+            }
+            else
+            {
+                foreach (var item in validationResult.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+
+            }
+
+            return View();
+        }
+    }
+ 
+}
